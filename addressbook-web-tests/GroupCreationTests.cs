351f3e7270f205cb5bc59da795058feee3706c6d
Дмитрий Симоненко <dima.simonenko@gmail.com>
2025-06-01 01:11:48 +0800

    Первый работающий тест

---
+++ addressbook-web-tests/GroupCreationTests.cs
@@ -0,0 +1,141 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Diagnostics.Eventing.Reader;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class GroupCreationTests
+    {
+        private IWebDriver driver;
+        private string baseURL;
+        private StringBuilder verificationErrors;
+        private bool acceptNextAlert = true;
+       
+        private string loginUser;
+        private string passwordUser;
+
+        private string textGroup_name;
+        private string textGroup_header;
+        private string textGroup_footer;
+
+
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new ChromeDriver();
+            baseURL = "http://localhost/addressbook/";
+            verificationErrors = new StringBuilder();
+            
+            //данные пользователя для входа
+            loginUser = "admin";
+            passwordUser= "secret";
+            
+            //заполнение формы
+            textGroup_name = "Текст в поле Group name";
+            textGroup_header = "Текст в поле Group header (Logo)";
+            textGroup_footer = "Текст в поле Group footer (Comment)";
+
+
+            
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+
+        public void GroupCreationTest()
+        {
+            //авторизация
+            driver.Navigate().GoToUrl(baseURL);
+            driver.FindElement(By.Name("user")).Click();
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(loginUser);
+            driver.FindElement(By.Name("pass")).Click();
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(passwordUser);
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+
+            //создание группы
+            driver.FindElement(By.LinkText("groups")).Click();
+            driver.FindElement(By.Name("new")).Click();
+            driver.FindElement(By.Name("group_name")).Click();
+            driver.FindElement(By.Name("group_name")).Clear();
+            driver.FindElement(By.Name("group_name")).SendKeys(textGroup_name);
+            driver.FindElement(By.Name("group_header")).Click();
+            driver.FindElement(By.Name("group_header")).Clear();
+            driver.FindElement(By.Name("group_header")).SendKeys(textGroup_header);
+            driver.FindElement(By.Name("group_footer")).Click();
+            driver.FindElement(By.Name("group_footer")).Clear();
+            driver.FindElement(By.Name("group_footer")).SendKeys(textGroup_footer);
+            driver.FindElement(By.Name("submit")).Click();
+            driver.FindElement(By.LinkText("group page")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
